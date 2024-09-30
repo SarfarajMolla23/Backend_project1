301e328bcfefcd3443c3cfac77cff72072e942cd
SarfarajMolla23 <sarfarajmollah49@gmail.com>
2024-09-30 12:58:20 +0530

    connect to Mongo Atlas

---
+++ src/index.js
@@ -0,0 +1,62 @@
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path:'./env'
+})
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+
+    app.on("error", (error) => {
+      console.log("ERROR", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App listening on port ${process.env.PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Error: ", error);
+    throw err;
+  }
+})(); 
+
+[()()] This is called ife function
+*/
