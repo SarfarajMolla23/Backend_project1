c716e96b1d344028a25a64b272b7e330b60fc245
SarfarajMolla23 <sarfarajmollah49@gmail.com>
2024-10-01 19:27:04 +0530

    add jwt , bcrypt and aggregate pipelines installed

--- src/app.js
+++ src/app.js
@@ -15,6 +15,6 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.use(express.static("public")); // this static is used because if some files or pdf came to me i want to store it in server so we make a public folder that there is one public assets is there anyone can access so make this make this name public.
-app.use(cookieParser());
+app.use(cookieParser()); // we need to install cookie-parser for accessing cookies from user browser.
 
 export { app };
