565392f4260c6a09e90d0d707cdb507108b6bbf1
SarfarajMolla23 <sarfarajmollah49@gmail.com>
2024-10-02 12:55:39 +0530

    add chnages

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
    res.status(200).json({
-    message: "serve ok"
+    message: "server ok"
   })
 });
 export { registerUser };
