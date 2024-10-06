b0c9bed4fb2bd557e588892fcd5ef3048da0a351
SarfarajMolla23 <sarfarajmollah49@gmail.com>
2024-10-06 21:35:23 +0530

    add watch history endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,6 +8,7 @@ import {
   getCloudinaryPublicId,
   deleteFromCloudinary,
 } from "../utils/deleteOldFile.js";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -413,15 +414,65 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
   ]);
   console.log(channel);
 
-  if(!channel?.length){
-      throw new ApiError(404," channel does not exists")
+  if (!channel?.length) {
+    throw new ApiError(404, " channel does not exists");
   }
-  
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User channel fetched successfully")
+    );
+});
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
   return res
   .status(200)
   .json(
-    new ApiResponse(200,channel[0],"User channel fetched successfully")
-)
+    new ApiResponse(200,user[0].watchHistory,"Watch history fetched successfully")
+  )
 });
 
 export {
@@ -435,4 +486,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,33 +1,59 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
-import { upload } from '../middlewares/multer.middleware.js'
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-
-
 
 const router = Router();
 
 router.route("/register").post(
-upload.fields([
+  upload.fields([
     {
       name: "avatar",
-      maxCount: 1
+      maxCount: 1,
     },
     {
-      name:"coverImage",
-      maxCount: 1
-    }
-]),  
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
   registerUser
-)
+);
 
-router.route("/login").post(loginUser)
-
+router.route("/login").post(loginUser);
 
 // secured routes
-router.route("/logout").post( verifyJWT, logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/update-coverImage")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watchhistory").get(verifyJWT, getWatchHistory);
 
 export default router;
-
 
